aec5fe66d0d93d9fd7c55aca6c37118416243bf0
ms-online <27732357@qq.com>
2021-01-04 16:41:04 +0800

    lesson-1

---
+++ main.js
@@ -0,0 +1,11 @@
+const nemo = ['nemo']
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('尼莫找到了！')
+    }
+  }
+}
+
+findNemo(nemo)
