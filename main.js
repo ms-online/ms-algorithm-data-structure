25e51336ad64e1f2213e99b96106b6f7c7a98fc7
ms-online <27732357@qq.com>
2021-01-05 20:39:04 +0800

    lesson-11

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-function printFirstItemThenFirstHalfThenSayHi100Times(items) {
-  console.log(items[0])
+function compressBoxesTwice(boxes, boxes2) {
+  boxes.forEach(function (boxes) {
+    console.log(boxes)
+  })
 
-  var middleIndex = Math.floor(items.length / 2)
-  var index = 0
-
-  while (index < middleIndex) {
-    console.log(items[index])
-    index++
-  }
-
-  for (var i = 0; i < 100; i++) {
-    console.log('hi')
-  }
+  boxes2.forEach(function (boxes) {
+    console.log(boxes)
+  })
 }
 
-//大O符号： O(1 + n/2 + 100) --> O(n/2 + 101) --> O( n/2 +1) --> O(n + 1) --> O(n)  线性时间复杂度
+// 大O符号：O(a+ b)
