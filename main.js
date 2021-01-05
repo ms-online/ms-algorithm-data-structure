779b5177759997acdb0330b9967a7bfa265cdded
ms-online <27732357@qq.com>
2021-01-05 16:45:51 +0800

    lesson-7

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-//随堂练习（一） ：推导下方函数的大O符号（可以一行一行判断）
-function funChallenge(input) {
-  let a = 10 //O(1)
-  a = 50 + 3 //O(1)
+//随堂练习（二） ：推导下方函数的大O符号（可以一行一行判断）
 
-  for (let i = 0; i < input.length; i++) {
+function anotherFunChallenge(input) {
+  let a = 5 //O(1)
+  let b = 10 //O(1)
+  let c = 50 //O(1)
+  for (let i = 0; i < input; i++) {
     //O(n)
-    anotherFunction() //O(n)
-    let stranger = true //O(n)
-    a++ //O(n)
+    let x = i + 1 //O(n)
+    let y = i + 2 //O(n)
+    let z = i + 3 //O(n)
   }
-  return a //O(1)
+  for (let j = 0; j < input; j++) {
+    //O(n)
+    let p = j * 2 //O(n)
+    let q = j * 2 //O(n)
+  }
+  let whoAmI = "I don't know" //O(1)
 }
 
-// 大O符号：3 + 4n --> O(3+4n) --> O(n)
+//大O符号：O(4 + 7n)
